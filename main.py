a8af3e4157bfd79581faeaa0a67c353b4647369c
Zachary Clute <zach@Richards-iMac.local>
2017-03-11 14:12:03 -0500

    troubleshooting vnev

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from kivy.config import Config
-Config.set('graphics', 'maxfps', '30')
+#from kivy.config import Config
+#Config.set('graphics', 'maxfps', '30')
 
 from audiostream import get_output
 from audiostream.sources.wave import SineSource
@@ -10,12 +10,26 @@ INCSIZE = 512
 
 from kivy.app import App
 from kivy.uix.slider import Slider
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MenuScreen(Screen):
+    pass
+
+class DemoScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(DemoScreen(name='demo'))
+sm.add_widget(MenuScreen(name='menu'))
 
 class AudioApp(App):
     def build(self):
         self.stream = get_output(channels=CHANNELS, buffersize=BUFSIZE, rate=22050)
         self.slider = Slider(min=110, max=880, value=440)
         self.slider.bind(value=self.update_freq)
+
+        self.button = Button(text="Left", font_size = 14)
 
         self.source = SineSource(self.stream, 440)
         self.source.start()
@@ -28,4 +42,5 @@ class AudioApp(App):
             self.source.frequency = value
 
 
-AudioApp().run()
+if __name__ == '__main__':
+    AudioApp().run()
