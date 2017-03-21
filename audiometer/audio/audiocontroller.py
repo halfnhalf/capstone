cab793c2cf608b9e175e2c2ad3c967649b79b592
Zachary Clute <zachclute@gmail.com>
2017-03-21 03:37:51 -0400

    moved test procedure to hearing test package. Added functions to audiocontroller. Added screen transition. Added dependency injection

--- audiometer/audio/audiocontroller.py
+++ audiometer/audio/audiocontroller.py
@@ -1,70 +1,31 @@
-#import time
+from audiostream import get_output
+from audiostream.sources.wave import SineSource
+
+CHANNELS = 2
+BUFSIZE = 2048
+INCSIZE = 512
 
 class AudioController:
-	def __init__(self):
-		# super(ClassName, self).__init__()
-		# self.arg = arg
-		# self.test_freqs = [250, 500, 1000, 2000, 4000, 8000]
-		# self.leftThresholds = []
-		# self.rightThresholds = []
+    sound_is_playing = False
+
+    def __init__(self, **kwargs):
+        pass
 
     def mute_left_channel(self):
         pass
 
- #    def start_test_sequence(self):
- #    	for freq in test_freqs:
- #    		self.leftThresholds.append(find_threshold(freq))
+    def toggle_sound(self, instance):
+        if not AudioController.sound_is_playing:
+            stream = get_output(channels=CHANNELS, buffersize=BUFSIZE, rate=22050)
+            self.source = SineSource(stream, 440)
+            self.source.start()
+        else:
+            self.source.stop()
 
- #    	for freq in test_freqs:
- #    		self.rightThresholds.append(find_threshold(freq))
-    		
+        AudioController.sound_is_playing = not AudioController.sound_is_playing
 
- #    def find_threshold(self, freq):
- #    	amps = []
-	# 	#Start at 10db below lowest threshold indicated during familiarization
-	# 	lastResponse = 10
-	# 	while len(amps) < 5:
-	# 		lastResponse = ascend(freq, lastResponse)
-	# 		if lastResponse in amps:
-	# 			#found our threshold for this freq
-	# 			return lastResponse
-
-	# 		amps.append(lastResponse)
-	# 		lastResponse = descend(freq, lastResponse)
-
-	# 	#If we made it out of loop, then could not get the same threshold twice out of 5 ascents
-	# 	#Present a test tone at a level 10 dB higher than the level of the last response.
-	# 	lastResponse = descend(freq, amps[4])
-	# 	return ascend(freq, lastResponse)
-
-
- #    def ascend(self, freq, amp):
- #    	#Go up by increments of 5 until button pressed
- #    	while not button_pressed():
-	# 		play_freq(freq, amp)
-	# 		time.sleep(2)
-	# 		stop_freq()
-	# 		amp = amp + 5
-	# 		if amp == 120:
-	# 			break
-
-	# 	return amp
-
-	# def descend(self, freq, amp):
-	# 	#Go down in increments of 10 until no button press
-	# 	play_freq(freq, amp)
-	# 	while button_pressed():
-	# 		amp = amp - 10
-	# 		play_freq(freq, amp)
-	# 		time.sleep(2)
-	# 		stop_freq()
-	# 		if amp < 10:
-	# 			break
-
-	# 	return amp
-
- #    def play_freq(self, freq, amp):
- #    	pass
-	
-	# def stop_freq(self):
-	# 	pass
+    def update_freq(self, slider, value):
+        #value = int(value / 50) * 50
+        if AudioController.sound_is_playing:
+            if value != self.source.frequency:
+                self.source.frequency = value
