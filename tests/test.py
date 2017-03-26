71cf39e7e0aa97e28a6358d4dda00003f84b1960
Zachary Clute <zachclute@gmail.com>
2017-03-26 02:50:33 -0400

    added tests

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+from audiometer.audio.audiocontroller import AudioController
+from time import sleep
+
+def main():
+    audio_controller = AudioController()
+
+    print "testing default arguments"
+    audio_controller.play_sound()
+    sleep(2)
+
+    print "testing frequency arguments"
+    audio_controller.play_sound(frequencies=[(400,.1),(400,.1)])
+    sleep(2)
+
+    print "testing different volumes"
+    audio_controller.play_sound(frequencies=[(400,.2),(400,.5)])
+    sleep(2)
+
+    print "testing different frequencies"
+    audio_controller.play_sound(frequencies=[(800,.2),(400,.2)])
+
+if __name__ == '__main__':
+    main()
+
