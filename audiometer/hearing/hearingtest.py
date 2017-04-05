d83414de1203231a1d727642c1055c662dd542d4
connerza <connerza@vcu.edu>
2017-04-05 15:21:13 -0400

    uncommenting hearingtest

--- audiometer/hearing/hearingtest.py
+++ audiometer/hearing/hearingtest.py
@@ -2,66 +2,78 @@ import time
 
 class HearingTest:
     def __init__(self, **kwargs):
-	# super(ClassName, self).__init__()
-	# self.arg = arg
-	# self.test_freqs = [250, 500, 1000, 2000, 4000, 8000]
-	# self.leftThresholds = []
-	# self.rightThresholds = []
+		super(ClassName, self).__init__()
+		self.audiometer = kwargs['audiometer']
+        self.audio_controller = self.audiometer.audio_controller
+		self.test_freqs = [250, 500, 1000, 2000, 4000, 8000]
+		self.leftThresholds = []
+		self.rightThresholds = []
+		self.buttonPressed = False
 
- #    def start_test_sequence(self):
- #    	for freq in test_freqs:
- #    		self.leftThresholds.append(find_threshold(freq))
+    def start_test_sequence(self):
+    	for freq in test_freqs:
+    		self.leftThresholds.append(find_threshold(freq, True))
 
- #    	for freq in test_freqs:
- #    		self.rightThresholds.append(find_threshold(freq))
+    	for freq in test_freqs:
+    		self.rightThresholds.append(find_threshold(freq, False))
     		
 
- #    def find_threshold(self, freq):
- #    	amps = []
-	# 	#Start at 10db below lowest threshold indicated during familiarization
-	# 	lastResponse = 10
-	# 	while len(amps) < 5:
-	# 		lastResponse = ascend(freq, lastResponse)
-	# 		if lastResponse in amps:
-	# 			#found our threshold for this freq
-	# 			return lastResponse
+    def find_threshold(self, freq, side):
+    	amps = []
+		#Start at 10db below lowest threshold indicated during familiarization
+		lastResponse = 10
+		while len(amps) < 5:
+			lastResponse = ascend(freq, lastResponse)
+			if lastResponse in amps:
+				#found our threshold for this freq
+				return lastResponse
 
-	# 		amps.append(lastResponse)
-	# 		lastResponse = descend(freq, lastResponse)
+			amps.append(lastResponse)
+			lastResponse = descend(freq, lastResponse)
 
-	# 	#If we made it out of loop, then could not get the same threshold twice out of 5 ascents
-	# 	#Present a test tone at a level 10 dB higher than the level of the last response.
-	# 	lastResponse = descend(freq, amps[4])
-	# 	return ascend(freq, lastResponse)
+		#If we made it out of loop, then could not get the same threshold twice out of 5 ascents
+		#Present a test tone at a level 10 dB higher than the level of the last response.
+		lastResponse = descend(freq, amps[4])
+		return ascend(freq, lastResponse)
 
 
- #    def ascend(self, freq, amp):
- #    	#Go up by increments of 5 until button pressed
- #    	while not button_pressed():
-	# 		play_freq(freq, amp)
-	# 		time.sleep(2)
-	# 		stop_freq()
-	# 		amp = amp + 5
-	# 		if amp == 120:
-	# 			break
+    def ascend(self, freq, amp):
+    	#Go up by increments of 5 until button pressed
+    	while not buttonPressed:
+			self.play_freq(freq, amp, side)
+			time.sleep(2)
+			stop_freq()
+			amp = amp + 5
+			if amp == 120:
+				break
 
-	# 	return amp
+		buttonPressed = False
+		return amp
 
-	# def descend(self, freq, amp):
-	# 	#Go down in increments of 10 until no button press
-	# 	play_freq(freq, amp)
-	# 	while button_pressed():
-	# 		amp = amp - 10
-	# 		play_freq(freq, amp)
-	# 		time.sleep(2)
-	# 		stop_freq()
-	# 		if amp < 10:
-	# 			break
+	def descend(self, freq, amp):
+		#Go down in increments of 10 until no button press
+		self.play_freq(freq, amp, side)
+		while buttonPressed:
+			buttonPressed = False
+			amp = amp - 10
+			self.play_freq(freq, amp, side)
+			time.sleep(2)
+			stop_freq()
+			if amp < 10:
+				break
 
-	# 	return amp
+		return amp
 
- #    def play_freq(self, freq, amp):
- #    	pass
+	def button_press(self):
+		self.buttonPressed = True
+    
+    def play_freq(self, freq, amp, side):
+    	if side:
+    		#Left side
+    		self.audio_controller.play_sound(frequencies=[(freq, amp),(freq,0)], duration=2)
+    	else:
+    		#Right side
+    		self.audio_controller.play_sound(frequencies=[(freq, 0),(freq, amp)], duration=2)
 	
-	# def stop_freq(self):
-	#
+	def stop_freq(self):
+		self.audio_controller.stop_sound()
---
+++ test.py
@@ -0,0 +1,63 @@
+from audiometer.audio.audiocontroller import AudioController
+from time import sleep
+import sys, getopt
+
+def main(argv):
+    channels = 2
+    try:
+        opts, args = getopt.getopt(argv, "c:")
+    except: 
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-c":
+            channels = int(arg)
+
+    audio_controller = AudioController()
+
+    if channels == 2:
+        print "right muted"
+        audio_controller.play_sound(frequencies=[(400,.5),(0,.5)])
+        sleep(2)
+        audio_controller.stop_sound()
+
+        print "testing different volumes"
+        audio_controller.play_sound(frequencies=[(400,.2),(400,.5)])
+        sleep(2)
+        audio_controller.stop_sound()
+
+        print "testing different frequencies"
+        audio_controller.play_sound(frequencies=[(8000,.2),(400,.2)])
+        sleep(2)
+        audio_controller.stop_sound()
+
+        print "testing infinite tone"
+        audio_controller.play_sound(frequencies=[(8000,.2),(400,.2)], duration=-1)
+        sleep(2)
+        audio_controller.update_tones(frequencies=[(800,.2),(400,.2)])
+        sleep(2)
+        audio_controller.stop_sound()
+
+    elif channels == 4:
+        print "right muted"
+        audio_controller.play_sound(frequencies=[(400,.8),(0,.8),(400,.8),(0,.8)])
+        sleep(2)
+	audio_controller.stop_sound()
+
+        print "testing different volumes"
+        audio_controller.play_sound(frequencies=[(400,.8),(400,.5),(400,.8),(400,.5)])
+        sleep(2)
+	audio_controller.stop_sound()
+
+        print "testing different frequencies"
+        audio_controller.play_sound(frequencies=[(400,.8),(8000,.8),(400,.8),(8000,.8)])
+        sleep(2)
+	audio_controller.stop_sound()
+
+    else:
+        print "bad channels"
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
