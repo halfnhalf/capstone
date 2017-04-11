3ca308b9a4190e1ff5779403302971360fd3435e
connerza <connerza@vcu.edu>
2017-04-11 19:02:19 -0400

    initial functionality for hearing test and hearing screen

--- audiometer/screens/hearingscreen.py
+++ audiometer/screens/hearingscreen.py
@@ -2,7 +2,8 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.base import runTouchApp
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from audiometer.hearing.hearingtest import HearingTest
 
 class HearingScreen(Screen):
     def __init__(self, **kwargs):
@@ -10,6 +11,19 @@ class HearingScreen(Screen):
         self.audiometer = kwargs['audiometer']
         self.audio_controller = self.audiometer.audio_controller
         self.screen_manager = self.audiometer.root
+        self.test = HearingTest(audiometer=self.audiometer)
+
+        layout = BoxLayout(orientation='vertical')
+        heard_button = Button(text="I hear it!", font_size=50)
+        start_button = Button(text="Start Test!", font_size=50)
+        heard_button.bind(on_press=self.test.button_press)
+        start_button.bind(on_press=self.test.start_test_sequence)
+        layout.add_widget(start_button)
+        layout.add_widget(heard_button)
+        self.add_widget(layout)
+
+
+
 
 
 #dropdown = DropDown()
