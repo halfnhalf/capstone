b643a5e996518ebdf4f68da360c7c828a09c4331
Zachary Clute <zachclute@gmail.com>
2017-04-20 17:45:21 -0400

    improved sound generation by splitting chunks generated into portions (as a ratio of num channels). Need to test on the pi but this should cut down computation time by a fouth.

--- audiometer/audio/tone.py
+++ audiometer/audio/tone.py
@@ -6,7 +6,7 @@ import numpy as np
 from array import array
 
 #sampling rate, Hz, must be integer
-RATE = 22050*2
+RATE = 48000#22050*2
 #how large we want our pcm chunks to be
 BUFSIZE = 256*20
 SAMPWIDTH = 2
@@ -28,8 +28,7 @@ class SineWave():
     def generate_period(self):
         seconds_per_period = np.reciprocal(self.frequency) if self.frequency > 0 else 1
         self.period = self.volume*np.sin(2*np.pi*self.frequency*np.linspace(0, seconds_per_period, num=self.samples_per_period))
-        #print self.period
-        #self.period = np.tile(self.period, int(BUFSIZE*2/self.samples_per_period))
+        #print self.period, len(self.period), seconds_per_period
 
 class Noise():
     def __init__(self, volume):
@@ -61,8 +60,7 @@ class Tones():
         self._generate_periods()
 
     def _generate_periods(self):
-        for channel in range(self.num_channels):
-            this_freq = self.frequencies[channel]
+        for this_freq in self.frequencies:
             if this_freq[0] > 0:
                 self.sounds.append(SineWave(this_freq[0], this_freq[1]))
             elif this_freq[1] == 0 or this_freq[0] == 0:
@@ -71,15 +69,27 @@ class Tones():
                 self.sounds.append(Noise(this_freq[1]))
 
     def callback(self, in_data, frame_count, time_info, status):
+        frame_count = frame_count/self.num_channels
         if not self.duration < 0 and self.position >= int((RATE * self.duration)/2):
             self.stop_sound()
 
         if not self.play_sound:
             callback_flag = pyaudio.paComplete
+            return (0, callback_flag)
         else:
             callback_flag = pyaudio.paContinue
 
-        return (self._get_chunk(frame_count), callback_flag)
+        #return (self._get_chunk(frame_count), callback_flag)
+        channel_chunks = []
+        for channel_sound in self.sounds:
+            if channel_sound is None: 
+                channel_chunks.append(MAX_AMP * np.arange(frame_count)*0)
+            else:
+                #print (MAX_AMP * channel_sound.period[np.remainder(np.arange(self.position, self.position+frame_count), channel_sound.samples_per_period)]).astype(int)
+                channel_chunks.append((MAX_AMP * channel_sound.period[np.remainder(np.arange(self.position, self.position+frame_count), channel_sound.samples_per_period)]))
+
+        self.position =  self.position + frame_count
+        return ((np.vstack(channel_chunks).reshape((-1,),order='F')).astype(np.int16).tostring(), callback_flag)
 
     def stop_sound(self):
         '''
@@ -101,22 +111,6 @@ class Tones():
 
         self._generate_periods()
         time.sleep(.2) #allow time to generate the periods before we're allowed to update again
-    
-    def _get_chunk(self, frame_count):
-
-        channel_chunks = []
-        for channel_sound in self.sounds:
-            if channel_sound is None: 
-                channel_chunks.append(MAX_AMP * np.arange(frame_count)*0)
-            else:
-                #print (MAX_AMP * channel_sound.period[np.remainder(np.arange(self.position, self.position+frame_count), channel_sound.samples_per_period)]).astype(int)
-                channel_chunks.append((MAX_AMP * channel_sound.period[np.remainder(np.arange(self.position, self.position+frame_count), channel_sound.samples_per_period)]).astype(np.int))
-
-        self.position =  self.position + frame_count
-        #print np.vstack((channel_chunks)).reshape((-1,),order='F').astype(np.int16)
-        return (np.vstack(channel_chunks).reshape((-1,),order='F')).astype(np.int16).tostring()
-
-
 
     def _get_chunk_quad(self, frame_count):
         data = array('h')
