9437dcc3fed0a977acffa0f6698d3951de7baa6a
James Le <jamesle@Jamess-Air.fios-router.home>
2017-04-21 01:56:14 -0400

    added results page with bone and air, just need json integration

--- audiometer/screens/resultsscreen.py
+++ audiometer/screens/resultsscreen.py
@@ -1,11 +1,11 @@
 import json
-from math import sin
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
 from kivy.uix.screenmanager import Screen
-from kivy.garden.graph import Graph, MeshLinePlot, SmoothLinePlot
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.dropdown import DropDown
-from kivy.uix.boxlayout import BoxLayout
-from kivy.base import runTouchApp
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+
 
 
 class ResultsScreen(Screen):
@@ -14,24 +14,47 @@ class ResultsScreen(Screen):
         self.audiometer = kwargs['audiometer']
         self.audio_controller = self.audiometer.audio_controller
         self.screen_manager = self.audiometer.root
-        self.layout = BoxLayout()
-        graph_theme = {'background_color':'f8f8f2'}
-        graph = Graph(xlabel='Frequency', ylabel='Decibel', x_ticks_minor=4,
-			x_ticks_major=1000, y_ticks_major=10,
-			y_grid_label=True, x_grid_label=True, padding=5,
-			x_grid=True, y_grid=True, xmin=0, xmax=8000, ymin=-110, ymax=10
-            )
-        plot = MeshLinePlot(color=[1,0,0,1])
-        plot.points = [(x, sin(x / 10.) ) for x in range(0, 101)]
-        graph.add_plot(plot)
-        #runTouchApp(graph)
-        self.add_widget(graph)
-        self.add_widget(self.layout)
-       
+        self.layout = FloatLayout(size=(800, 480))
+        #opens json file, change directory
+        with open('/Users/jamesle/Documents/capstone/data/test.json') as json_data:
+                results = json.load(json_data)
+        #sets both ear axes for air
+        air_left_x = results['air']['Left Ear'][0]['Frequencies']
+        air_left_y = results['air']['Left Ear'][0]['decibels']
+        air_right_x = results['air']['Right Ear'][0]['Frequencies']
+        air_right_y = results['air']['Right Ear'][0]['decibels']
+        #sets both ear axes for bone
+        bone_left_x = results['bone']['Left Ear'][0]['Frequencies']
+        bone_left_y = results['bone']['Left Ear'][0]['decibels']
+        bone_right_x = results['bone']['Right Ear'][0]['Frequencies']
+        bone_right_y = results['bone']['Right Ear'][0]['decibels']
+        ##plots air conduction
+        air = plt.figure()
+        air_graph = air.add_subplot(111)
+        plt.title('Air')
+        plt.xlabel('Frequency(Hz)')
+        plt.ylabel('decibel(dB)')
+        plt.ylim((60,0))
+        air_graph.plot(air_left_x,air_left_y, 'bx-', markersize = 12)
+        air_graph.plot(air_right_x,air_right_y, 'ro-', markersize =12)
+        plt.savefig('Air Conduction.png')
+        air_picture = Image(source='Air Conduction.png', size_hint = (0.5,1))
+        ## plots bone conduction
+        bone = plt.figure()
+        bone_graph = bone.add_subplot(111)
+        plt.title('Bone')
+        plt.xlabel('Frequency(Hz)')
+        plt.ylabel('decibel(dB)')
+        plt.ylim((60,0))
+        bone_graph.plot(bone_left_x,bone_left_y, 'bx-', markersize = 12)
+        bone_graph.plot(bone_right_x,bone_right_y, 'ro-', markersize =12)
+        plt.savefig('Bone Conduction.png')
+        bone_picture = Image(source='Bone Conduction.png', size_hint = (0.5,1), pos = (410,0))
 
-#with open('/Users/jamesle/Documents/capstone/data/test.json') as json_data:
-#   		results = json.load(json_data)
-# 
+        self.add_widget(air_picture)
+        self.add_widget(bone_picture)
+        self.add_widget(self.layout)
+
 #print "Frequencies: ", results['Left Ear'][0]['Frequencies']
 #print "Decibels ", results['Right Ear'][0]['decibels']
 
