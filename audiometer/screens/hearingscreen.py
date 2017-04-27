c83062852c8c08c64b0ae5188cd68f5399fedcfa
connerza <connerza@vcu.edu>
2017-04-27 09:10:59 -0400

    fixing issues with bind and unbind. stopping and starting tests should now work.

--- audiometer/screens/hearingscreen.py
+++ audiometer/screens/hearingscreen.py
@@ -21,18 +21,18 @@ class HearingScreen(Screen):
         self.heard_button = Button(text="I hear it!", color = (0,0,0,1),background_normal = "images/button.png",font_size=50,background_color = (0.9,0.9,0,1), size_hint=(.4, .4),pos = (240,230))
         self.start_button = Button(text="Start Test!", font_size=20, background_color = (0,1,0,1), size_hint=(.2, .1),pos = (320,100))
         back= Button(text = 'Instruction',size_hint=(.2, .1),font_size = 20,background_color = (1,0,0,1),pos = (140,100))
-        back.bind(on_release=self.back)
+        back.bind(on_press=self.back)
 
         home = Button(text="Home", font_size = 20, size_hint=(.2, .1),background_color = (1,0,0,1),pos = (500,100))
-        home.bind(on_release=self.home)
+        home.bind(on_press=self.home)
 
         self.vcurams = Image(source='./images/vcurams.png', size_hint = (0.2,0.2),pos = (630,-5))
 
         #self.ece = Image(source='./images/ece.png', size_hint = (0.25,0.25),pos = (580,5))
 
 
-        self.heard_button.bind(always_release=self.on_heard_press)
-        self.start_button.bind(always_release=self.on_start_press)
+        self.heard_button.bind(on_release=self.on_heard_press)
+        self.start_button.bind(on_release=self.on_start_press)
         self.layout.add_widget(self.start_button)
         self.layout.add_widget(self.heard_button)
         self.layout.add_widget(back)
@@ -42,19 +42,16 @@ class HearingScreen(Screen):
         #self.add_widget(self.ece)
 
     def on_start_press(self, instance):
+        print "GOT START PRESS CALL"
         #Start thread with test
         self.audiometer.test.stop.clear()
+        self.change_to_stop_button()
         threading.Thread(target=self.test_thread).start()
-        self.start_button.funbind('on_press', self.on_start_press)
-        self.start_button.fbind('on_press', self.on_stop_press)
-        self.start_button.text = "Stop Test!"
-        self.start_button.background_color = (0,0,1,1)
-
+        
     def on_stop_press(self, instance):
+        print "GOT STOP PRESS CALL"
         self.audiometer.test.stop_thread()
-        self.start_button.funbind('on_press', self.on_stop_press)
-        self.start_button.fbind('on_press', self.on_start_press)
-        self.start_button.text = "Start Test!"
+        self.change_to_start_button()
         
     def on_heard_press(self, instance):
         self.audiometer.test.button_press()
@@ -65,12 +62,22 @@ class HearingScreen(Screen):
         #Leave page
         self.thread_ended_go_to_results()
 
-    @mainthread
-    def thread_ended_go_to_results(self):
-        self.start_button.funbind('on_press', self.on_stop_press)
-        self.start_button.fbind('on_press', self.on_start_press)
+    def change_to_stop_button(self):
+        print "CHANGING TO STOP BUTTON"
+        self.start_button.unbind(on_release = self.on_start_press)
+        self.start_button.bind(on_release = self.on_stop_press)
+        self.start_button.text = "Stop Test!"
+        self.start_button.background_color = (0,0,1,1)
+
+    def change_to_start_button(self):
+        print "CHANGING TO START BUTTON"
+        self.start_button.unbind(on_release = self.on_stop_press)
+        self.start_button.bind(on_release = self.on_start_press)
         self.start_button.text = "Start Test!"
         self.start_button.background_color = (0,1,0,1)
+
+    @mainthread
+    def thread_ended_go_to_results(self):
         self.audiometer.root.get_screen('results').result_button_pressed('current_audiogram.json')
         self.screen_manager.current = 'results'
 
@@ -81,4 +88,10 @@ class HearingScreen(Screen):
     def home(self, instance):
         self.screen_manager.current = 'home'
         self.screen_manager.transition.direction='right'
+
+    def on_leave(self):
+        self.audiometer.test.stop_thread()
+        self.change_to_start_button()
+
+        
 
